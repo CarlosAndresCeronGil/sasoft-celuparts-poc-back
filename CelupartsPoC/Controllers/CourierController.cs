45ee931244c9772c4b8d142c50746c85eab65a66
CarlosAndresCeronGil <andrescg0106@hotmail.com>
2022-08-05 11:33:39 -0500

    Controladores de mensajero y pago de reparacion

--- CelupartsPoC/Controllers/CourierController.cs
+++ CelupartsPoC/Controllers/CourierController.cs
@@ -39,5 +39,38 @@ namespace CelupartsPoC.Controllers
 
             return Ok(await _context.Courier.ToListAsync());
         }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Courier>>> UpdateTechnician(Courier request)
+        {
+            var dbCourier = _context.Courier.FindAsync(request.IdCourier);
+            if (dbCourier.Result == null)
+            {
+                return BadRequest("Courier not found!");
+            }
+            dbCourier.Result.Names = request.Names;
+            dbCourier.Result.Surnames = request.Surnames;
+            dbCourier.Result.Phone = request.Phone;
+            dbCourier.Result.Email = request.Email;
+            dbCourier.Result.Password = request.Password;
+            dbCourier.Result.AccountStatus = request.AccountStatus;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Courier.ToListAsync());
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<List<Courier>>> Delete(int id)
+        {
+            var dbCourier = _context.Courier.FindAsync(id);
+            if (dbCourier.Result == null)
+            {
+                return BadRequest("Courier not found!");
+            }
+            _context.Courier.Remove(dbCourier.Result);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.Technician.ToListAsync());
+        }
     }
 }
---
+++ CelupartsPoC/Controllers/RepairPaymentController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CelupartsPoC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RepairPaymentController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public RepairPaymentController(DataContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<RepairPayment>>> Get()
+        {
+            return Ok(await _context.RepairPayment.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<RepairPayment>> Get(int id)
+        {
+            var repairPayment = _context.RepairPayment.FindAsync(id);
+            if (repairPayment.Result == null)
+            {
+                return BadRequest("Repair payment not found!");
+            }
+            return Ok(repairPayment.Result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<RepairPayment>>> AddRepairPayment(RepairPayment repairPayment)
+        {
+            _context.RepairPayment.Add(repairPayment);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.RepairPayment.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<RepairPayment>>> UpdateRepairPayment(RepairPayment request)
+        {
+            var dbRepairPayment = _context.RepairPayment.FindAsync(request.IdRepairPayment);
+            if (dbRepairPayment.Result == null)
+            {
+                return BadRequest("Repair payment not found!");
+            }
+            dbRepairPayment.Result.IdRepair = request.IdRepair;
+            dbRepairPayment.Result.PaymentMethod = request.PaymentMethod;
+            dbRepairPayment.Result.BillPayment = request.BillPayment;
+            dbRepairPayment.Result.PaymentDate = request.PaymentDate;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.RepairPayment.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<RepairPayment>>> Delete(int id)
+        {
+            var dbRepairPayment = _context.RepairPayment.FindAsync(id);
+            if (dbRepairPayment.Result == null)
+            {
+                return BadRequest("Repair payment not found!");
+            }
+            _context.RepairPayment.Remove(dbRepairPayment.Result);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.RepairPayment.ToListAsync());
+        }
+    }
+}
